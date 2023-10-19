a8b82529f74b9adfac5f24ab9b519168b9a95293
Bin Budiman Affandi <b.binbudimanaffandi@dxc.com>
2023-10-19 11:42:09 +0800

    finish text generate functionalitycreate

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
 import data from "./data";
+import { nanoid } from "nanoid";
  
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0,amount));
+  }
+
   return <section>
     <h4>tired of boring lorem ipsum?</h4>
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="amount">paragraphs: </label>
       <input type="number" 
       id="amount" 
       name="amount" 
-      value={count}
       min="1" 
       step="1" 
-      max="8"></input>
-      <button type="submit" onChange={(e) => setCount(e.target.value)}>generate</button>
+      max="8"
+      value={count}
+      onChange={(e) => setCount(e.target.value)}>
+      </input>
+      <button type="submit">generate</button>
     </form>
+    <article>
+      {text.map((item) => {
+        return <p key={nanoid}>{item}</p>
+      })}
+    </article>
   </section>;
 };
 export default App;
