4e14273c4744c12d78fc99ab96da46e9de906b7d
Bin Budiman Affandi <b.binbudimanaffandi@dxc.com>
2023-10-19 10:09:13 +0800

    create basic outline of the text generate form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import data from "./data";
+ 
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  return <section>
+    <h4>tired of boring lorem ipsum?</h4>
+    <form>
+      <label htmlFor="amount">paragraphs: </label>
+      <input type="number" 
+      id="amount" 
+      name="amount" 
+      value={count}
+      min="1" 
+      step="1" 
+      max="8"></input>
+      <button type="submit" onChange={(e) => setCount(e.target.value)}>generate</button>
+    </form>
+  </section>;
 };
 export default App;
